77c2066d5bf28ee0ea51e70f69cedd63ce8fd13a
MinkyeongSung <wildlegion59@gmail.com>
2023-09-19 16:50:24 +0900

    주문정보 조회완료

--- src/main/java/com/example/kakao/order/OrderResponse.java
+++ src/main/java/com/example/kakao/order/OrderResponse.java
@@ -1,5 +1,8 @@
 package com.example.kakao.order;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -7,11 +10,23 @@ import lombok.ToString;
 public class OrderResponse {
 
     // (기능4) 주문상품 정보조회 (유저별)
+
     @ToString
     @Getter
     @Setter
     public static class FindAllByUserDTO {
+        private ArrayList<CartDTO> carts;
+        private Integer totalPrice;
 
+        @ToString
+        @Getter
+        @Setter
+        public static class CartDTO {
+            private Integer cartId;
+            private String optionName;
+            private Integer price;
+            private Integer quantity;
+        }
     }
 
     // (기능5) 주문결과 확인
