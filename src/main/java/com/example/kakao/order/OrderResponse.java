cadf82e3c0ccbfe04841d1db8a775a3ec5a956e8
muaga <more9494@naver.com>
2023-09-19 16:59:05 +0900

    dev 이동전 저장

--- src/main/java/com/example/kakao/order/OrderResponse.java
+++ src/main/java/com/example/kakao/order/OrderResponse.java
@@ -1,5 +1,12 @@
 package com.example.kakao.order;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.kakao.cart.Cart;
+import com.example.kakao.order.item.Item;
+import com.example.kakao.product.Product;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -20,5 +27,46 @@ public class OrderResponse {
     @Setter
     public static class FindByIdDTO {
 
+        private int orderId;
+        private List<OrderItemDTO> orderItemDTOList;
+        private int totalPrice;
+
+        public FindByIdDTO(Item item, List<OrderItemDTO> orderItemDTO, int totalPrice) {
+            this.orderId = item.getOrder().getId();
+            this.orderItemDTOList = orderItemDTOList;
+            this.totalPrice = totalPrice;
+        }
+    }
+
+    @ToString
+    @Getter
+    @Setter
+    public class OrderItemDTO {
+        private int productId;
+        private String productName;
+        private List<OptionItemDTO> optionItemDTOList;
+
+        public OrderItemDTO(Product product, List<OptionItemDTO> optionItemDTOList) {
+            this.productId = product.getId();
+            this.productName = product.getProductName();
+            this.optionItemDTOList = optionItemDTOList;
+        }
+    }
+
+    @ToString
+    @Getter
+    @Setter
+    public class OptionItemDTO {
+        private int optionId;
+        private String optionName;
+        private int quantity;
+        private int price;
+
+        public OptionItemDTO(Item item) {
+            this.optionId = item.getOption().getId();
+            this.optionName = item.getOption().getOptionName();
+            this.quantity = item.getQuantity();
+            this.price = item.getPrice();
+        }
     }
 }
